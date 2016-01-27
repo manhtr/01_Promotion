f5d0f19fc34a86a1ee46ede29dd6c990687f3c65
Tran Manh <manhtr@gmail.com>
2016-01-27 22:56:32 +0700

    Source code
    Source code chương trình cấp mã dự thưởng

---
+++ Promotion/Promotion/Account/Login.aspx.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.DirectoryServices;
+using System.Web.UI;
+//using Promotion.Commons;
+using log4net;
+
+namespace Promotion.Account
+{
+    public partial class Login : Page
+    {
+        ILog log = LogManager.GetLogger(typeof(Login));
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                if (Session[Commons.Constant.USERNAME] != null && !string.IsNullOrEmpty(Session[Commons.Constant.USERNAME].ToString()))
+                {
+                    Page.Response.Redirect(Commons.Common.GetRootRequest() + "Default.aspx");
+                }
+
+                if (!IsPostBack)
+                {
+                    txtUsername.Focus();
+                }
+            }
+        }
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            lblInfor.Text = string.Empty;
+
+            if (string.IsNullOrEmpty(txtUsername.Text))
+            {
+                lblInfor.Text = "Nhập tên đăng nhập.";
+                txtUsername.Focus();
+                return;
+            }
+
+            if (string.IsNullOrEmpty(txtPassword.Text))
+            {
+                lblInfor.Text = "Nhập mật khẩu.";
+                txtPassword.Focus();
+                return;
+            }
+            //if (ValidateByAD(txtUsername.Text.Trim(), txtPassword.Text, "g-bank.com.vn"))
+            if (ValidateUser(txtUsername.Text.Trim(), txtPassword.Text))
+            {
+                using (DataModel.STAFF_USER_BO objSTAFF_USER_BO = new DataModel.STAFF_USER_BO())
+                {
+                    DataModel.STAFF_USER objSTAFF_USER = objSTAFF_USER_BO.GetByUSERNAME(txtUsername.Text);
+                    if (objSTAFF_USER != null)
+                    {
+                        Session[Commons.Constant.USERNAME] = txtUsername.Text.Trim().ToLower();
+                        Session[Commons.Constant.FULLNAME] = objSTAFF_USER.HO_TEN;
+                        Session[Commons.Constant.DAO_CODE] = objSTAFF_USER.DAO_CODE;
+                        Session[Commons.Constant.DEPT_CODE] = objSTAFF_USER.DEPT_CODE;
+                        log.Info("Login success: " + txtUsername.Text.Trim());
+                        //Page.Response.Redirect(Common.GetRootRequest() + "Default.aspx");
+                        using (DataModel.User_BO objUser_BO = new DataModel.User_BO())
+                        {
+                            DataModel.User objUser = objUser_BO.GetByUserName(Session[Commons.Constant.USERNAME].ToString());
+                            if (objUser != null && objUser.Permisions != null)
+                            {
+                                bool hasPer = false;
+                                foreach (Promotion.DataModel.Permision item in objUser.Permisions)
+                                {
+                                    if (item != null && item.Permision1 == Commons.Constant.PERMISION_ADMIN)
+                                    {
+                                        hasPer = true;
+                                        break;
+                                    }
+                                }
+                                if (hasPer)  //Admin
+                                {
+                                    Page.Response.Redirect(Commons.Common.GetRootRequest() + "Manager.aspx");
+                                }
+                                else  //Tellers
+                                {
+                                    Page.Response.Redirect(Commons.Common.GetRootRequest() + "Default.aspx");
+                                }
+                            }
+                        }
+
+                    }
+                    else
+                    {
+                        lblInfor.Text = "Người dùng không được truy cập vào hệ thống.";
+                        txtUsername.Focus();
+                        log.Info("Người dùng không được truy cập vào hệ thống");
+                    }
+                }
+            }
+            else
+            {
+                lblInfor.Text = "Tên đăng nhập và mật khẩu không phù hợp.";
+                txtUsername.Focus();
+                log.Info("Tên đăng nhập và mật khẩu không phù hợp. User: " + txtUsername.Text + ", password: " + txtPassword.Text);
+            }
+        }
+
+        private bool ValidateUser(string strUsername, string strPassword)
+        {
+            bool validateUser = false;
+            try
+            {
+                using (DataModel.User_BO objUser_BO = new DataModel.User_BO())
+                {
+                    DataModel.User objUser = objUser_BO.GetByUserName(strUsername, strPassword);
+                    if (objUser != null)
+                    {
+                        validateUser = true;
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                log.Error(ex.Message);
+            }
+            return validateUser;
+        }
+
+        private bool ValidateByAD(string strUsername, string strPassword, string strDomain)
+        {
+            strDomain = "10.1.1.10";
+            bool authentic = false;
+            try
+            {
+                DirectoryEntry entry = new DirectoryEntry("LDAP://" + strDomain, strUsername, strPassword);
+                object nativeObject = entry.NativeObject;
+                authentic = true;
+            }
+            catch (Exception ex)
+            {
+                authentic = false;
+            }
+            return authentic;
+        }
+    }
+}
---
+++ Promotion/Promotion/LiXi/XoaLiXi.aspx.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web.UI.WebControls;
+using Promotion.Commons;
+using Promotion.DataModel;
+using log4net;
+
+namespace Promotion.LiXi
+{
+    public partial class XoaLiXi : System.Web.UI.Page
+    {
+        ILog log = LogManager.GetLogger(typeof(XoaLiXi));
+
+        #region Events
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                txtCIF.Focus();
+            }
+        }
+
+        protected void cmdDelete_Click(object sender, EventArgs e)
+        {
+            lblMessage.Text = string.Empty;
+            if (string.IsNullOrEmpty(txtCIF.Text))
+            {
+                lblMessage.Text = "Nhập mã khách hàng";
+                txtCIF.Focus();
+                return;
+            }
+            if (string.IsNullOrEmpty(hdfCIF.Value))
+            {
+                lblMessage.Text = "Mã khách hàng không hợp lệ";
+                txtCIF.Focus();
+                return;
+            }
+
+            using (Promotion.DataModel.LiXi_BO objLiXi_BO = new Promotion.DataModel.LiXi_BO())
+            {
+                Promotion.DataModel.LiXi objLiXi = objLiXi_BO.GetByCIF(hdfCIF.Value);
+                if (objLiXi != null)
+                {
+                    objLiXi.IsUsed = false;
+                    objLiXi.Deleted = DateTime.Now;
+                    objLiXi.DeletedBy = Session[Promotion.Commons.Constant.USERNAME].ToString();
+                    bool result = objLiXi_BO.UpdateLiXi(objLiXi);
+                    if (result)
+                    {
+                        cmdDelete.Visible = false;
+                        lblMessage.Text = "Xóa cấp lì xì thành công.";
+                    }
+                    else
+                    {
+                        lblMessage.Text = "Có lỗi khi xóa cấp lì xì.";
+                    }
+                }
+                else
+                {
+                    lblMessage.Text = "Dữ liệu không hợp lệ";
+                }
+            }
+        }
+
+        protected void cmdSearch_Click(object sender, EventArgs e)
+        {
+            lblMessage.Text = string.Empty;
+
+            lblCIF.Text = string.Empty;
+            lblCustomerName.Text = string.Empty;
+            lblCMND.Text = string.Empty;
+            lblTelephone.Text = string.Empty;
+            lblAddress.Text = string.Empty;
+            lblAccountNumber.Text = string.Empty;
+            lblStatus.Text = string.Empty;
+
+            if (string.IsNullOrEmpty(txtCIF.Text))
+            {
+                lblMessage.Text = "Nhập mã khách hàng";
+                txtCIF.Focus();
+                return;
+            }
+
+            if (Session[Constant.USERNAME] != null)
+            {
+                using (PromotionEntities db = new PromotionEntities())
+                {
+                    var cust = db.CUSTOMERs.FirstOrDefault(t => t.CUSTOMER_CODE.Equals(txtCIF.Text));
+                    if (cust != null)
+                    {
+                        lblCIF.Text = cust.CUSTOMER_CODE;
+                        lblCustomerName.Text = cust.CUSTOMER_NAME;
+                        lblCMND.Text = cust.LEGAL_ID;
+                        if (!string.IsNullOrEmpty(cust.TELEPHONE))
+                        {
+                            lblTelephone.Text = cust.TELEPHONE;
+                        }
+                        if (!string.IsNullOrEmpty(cust.ADDRESS))
+                        {
+                            lblAddress.Text = cust.ADDRESS;
+                        }
+                        string strAccount = string.Empty;
+
+                        var listAcc = db.AZ_ACCOUNT.Where(t => t.CUSTOMER_ID.Equals(txtCIF.Text.Trim())).ToList();
+                        if (listAcc != null)
+                        {
+                            foreach (var azAccount in listAcc)
+                            {
+                                //strAccount += azAccount.ACCOUNT_NUMBER + "; ";
+                            }
+                        }
+
+                        lblAccountNumber.Text = strAccount;
+                        hdfCIF.Value = txtCIF.Text.Trim();
+                        using (DataModel.LiXi_BO objLiXi_BO = new DataModel.LiXi_BO())
+                        {
+                            DataModel.LiXi objLiXi = objLiXi_BO.GetByCIF(txtCIF.Text);
+                            if (objLiXi != null)
+                            {
+                                string strPGD = string.Empty;
+                                using (DataModel.COMPANY_BO objT24_COMPANY_BO = new DataModel.COMPANY_BO())
+                                {
+                                    DataModel.COMPANY objT24_COMPANY = objT24_COMPANY_BO.GetByCOMPANY_CODE(objLiXi.CoCode);
+                                    if (objT24_COMPANY != null)
+                                    {
+                                        strPGD = objT24_COMPANY.COMPANY_NAME;
+                                    }
+                                }
+                                lblStatus.Text = "Đã được cấp Lì xì vào " + objLiXi.Created.Value.ToString("dd/MM/yyyy HH:mm") + " tại " + strPGD + ". Cấp bởi " + objLiXi.CreatedBy;
+                                cmdDelete.Visible = true;
+                            }
+                            else
+                            {
+                                lblStatus.Text = "Khách hàng chưa được cấp Lì xì";
+                            }
+                        }
+                    }
+                    else
+                    {
+                        lblMessage.Text = "Thông tin khách hàng không phù hợp. ";
+                    }
+                }
+            }
+        }
+
+        #endregion
+
+        #region Methods
+
+        #endregion
+    }
+}
